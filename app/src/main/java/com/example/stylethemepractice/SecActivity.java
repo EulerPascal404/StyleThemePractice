ed35f0b4084c634605fbe880dfac7b487c9b5f0a
ThePuzzlr <90777621+ThePuzzlr@users.noreply.github.com>
2022-09-16 08:14:10 -0500

    9/16 commit

--- app/src/main/java/com/example/stylethemepractice/SecActivity.java
+++ app/src/main/java/com/example/stylethemepractice/SecActivity.java
@@ -2,21 +2,20 @@ package com.example.stylethemepractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sec);
     }
 
-    public void go(View view){
-        Intent intent = new Intent(MainActivity.this, SecActivity.class);
+    public void goBack(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
